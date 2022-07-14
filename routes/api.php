9d1a871cdb395b13078241c72f3a9cfae0024678
Drago Stevanovic <drago@sounder.fm>
2022-07-14 17:03:15 +0200

    Authentication

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/profile', [AuthController::class, 'getMyProfile'])->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
